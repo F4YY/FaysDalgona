f5712add86c5f63cbdee7e54d99d43f321c6285a
Riki Wendri <fayyadh.farizzi@gmail.com>
2023-12-12 14:38:11 +0700

    edit Dagona Mocaccino.js onClick event

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -74,8 +74,8 @@ function Main() {
   const {user, authReady} = useContext(AuthContext);
   const [showAlert, setShowAlert] = React.useState(false);
   const [feedbackValue, setFeedbackValue] = React.useState('');
-  const [menuPic, setMenuPic] = React.useState('Dalg_ChocSilv.jpg');
-  const [menuName, setMenuName] = React.useState('Dalgona Choco Silverqueen');
+  const [menuPic, setMenuPic] = React.useState(null);
+  const [menuName, setMenuName] = React.useState(null);
 
   const handleRateMenu = (e) => {
     if(user){
