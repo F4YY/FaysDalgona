46e19debce10464606cc8fe7ed37817ec6680ef2
Riki Wendri <fayyadh.farizzi@gmail.com>
2023-12-12 15:08:33 +0700

    edit Dagona Mocaccino.js onClick event

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -74,8 +74,8 @@ function Main() {
   const {user, authReady} = useContext(AuthContext);
   const [showAlert, setShowAlert] = React.useState(false);
   const [feedbackValue, setFeedbackValue] = React.useState('');
-  const [menuPic, setMenuPic] = React.useState(null);
-  const [menuName, setMenuName] = React.useState(null);
+  const [menuPic, setMenuPic] = React.useState(undefined);
+  const [menuName, setMenuName] = React.useState(undefined);
 
   const handleRateMenu = (e) => {
     if(user){
