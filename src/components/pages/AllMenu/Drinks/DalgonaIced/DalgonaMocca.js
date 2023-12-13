7e5cc4098f64a76ef65f06a3d0f17bd90edbed13
Riki Wendri <fayyadh.farizzi@gmail.com>
2023-12-13 08:20:00 +0700

    edit Main.js postresponse

--- src/components/pages/AllMenu/Drinks/DalgonaIced/DalgonaMocca.js
+++ src/components/pages/AllMenu/Drinks/DalgonaIced/DalgonaMocca.js
@@ -200,6 +200,8 @@ export const DalgonaMocca = ({
         </FeedbackArea>
         <SubmitRating
           onClick={() => {
+            console.log('setMenuPic', setMenuPic);
+            console.log('setMenuName', setMenuName);
             setMenuPic('Dalg_Moca.jpg');
             setMenuName(props.items[0].list[5].name);
             handleSubmit();
